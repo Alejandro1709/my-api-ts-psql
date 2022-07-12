35f2f7a60ee446a2c4240395182ba863f3c91b11
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-12 09:51:49 -0500

    Add: root route for the api

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello API');
+});
 
 app.listen(3030, () => console.log('Server is up and running on port 3030'));
