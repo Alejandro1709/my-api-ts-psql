8670dd335533d85e425cc26ca7e1e2c7ac528630
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-12 09:49:11 -0500

    Add json parser middleware

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(3030, () => console.log('Server is up and running on port 3030'));
